6d6d7e5a12a53f0c2ca556718b05e87524edbae0
garysen <garysen@163.com>
2025-11-26 17:24:53 +0800

    add video component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ProductViewer from "./components/ProductViewer";
 import gsap from "gsap";
 //滚动触发器
 import { ScrollTrigger } from "gsap/all";   
+import ShowCase from "./components/ShowCase";
 
 //gsap插件注册，注册后全局可用
 gsap.registerPlugin(ScrollTrigger);
@@ -14,6 +15,7 @@ const App = () => {
         <NavBar />
         <Hero />
         <ProductViewer />
+        <ShowCase />
     </main>
   )
 }
